1a934f73222cfd921d594fb5be7b2bb5e7b612c1
siddhraj <siddhrajraulji@gmail.com>
2022-04-01 12:40:24 +0530

    changes

--- src/component/home.js
+++ src/component/home.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import React from "react";
 
 export default function Home() {
   return (
-    <div>home</div>
-  )
+    <>
+      <div>home</div>
+      <div>home</div>
+    </>
+  );
 }
